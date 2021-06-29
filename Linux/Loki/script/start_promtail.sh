7b55d554a136cd74cb378cc5ce17b70c9b663fda
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2021-06-29 19:58:40 +0800

    Update start_promtail.sh

--- Linux/Loki/script/start_promtail.sh
+++ Linux/Loki/script/start_promtail.sh
@@ -10,6 +10,7 @@ function log_err() {
   echo -e "\033[31m$DATE [ERROR]: $1 \033[0m"
 }
 
+pkill promtail
 nohup ./bin/promtail-linux-amd64 -config.file=conf/config-promtail.yml  > running.log 2>&1 &
 
  [[ "$?" == "0" ]] && log_info "\n1.成功於背景執行Promtail\n2.停止請執行sh stop_promtail.sh\n3.執行 Log 進度，請使用tail -f running.log" || log_err "執行異常，請查看 running.log"
