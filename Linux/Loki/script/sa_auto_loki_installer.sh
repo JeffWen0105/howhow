972f0611d5a0481216c3385682124f0df6d64130
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2021-06-29 21:53:01 +0800

    update

--- Linux/Loki/script/sa_auto_loki_installer.sh
+++ Linux/Loki/script/sa_auto_loki_installer.sh
@@ -17,7 +17,7 @@ function check_release(){
     exit 1
   else
     os=$(hostnamectl | grep 'Operating System' | grep "CentOS")
-      [[ $os != '' ]] && log_info "檢測版本為CentOS 系列.." || log_info "檢測版本為Red Hat 系列.."
+      [[ $os != '' ]] && log_info "檢測版本為 CentOS 系列.." || log_info "檢測版本為 Red Hat 系列.."
   fi
 }
 
@@ -29,15 +29,14 @@ function docker_check(){
   [[ "$?" == "1" ]] && docker_compose_installer
 }
 
-
 function docker_installer() {
-  log_info "開始安裝Docker程序..請稍等"
+  log_info "開始安裝 Docker 程序..請稍等"
   sudo yum install -y yum-utils
   sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
   sudo yum install docker-ce docker-ce-cli containerd.io -y
-  log_info "Dokcer安裝完畢，並啟動服務及開機自動載入..."
+  log_info " Dokcer 安裝完畢，並啟動服務及開機自動載入..."
   sudo systemctl start docker
   sudo systemctl enable docker
   log_info "執行Hello World image"
@@ -45,7 +44,7 @@ function docker_installer() {
 }
 
 function docker_compose_installer(){
-  log_info "安裝Docker Compose..請稍等"
+  log_info "安裝 Docker Compose ..請稍等"
   sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" \
    -o /bin/docker-compose && \
   sudo chmod +x /bin/docker-compose
@@ -53,7 +52,7 @@ function docker_compose_installer(){
 }
 
 function loki_installer(){
-  log_info "部署Loki及Grafana..請稍等"
+  log_info "部署 Loki 及 Grafana ..請稍等"
   mkdir -p ~/howhow/loki &&  \
   sudo mkdir -p /mnt/rawdata/howhow/loki/conf  && \
   cd ~/howhow/loki && \
@@ -69,36 +68,37 @@ function loki_installer(){
 }
 
 function loki_init(){
-  log_info "初始化promtail..."
+  log_info "初始化 Promtail ..."
   lib_check
-  cd ~/howhow/loki
-  mkdir -p  promtail/bin && mkdir -p promtail/log && mkdir -p promtail/conf
+  howhowhome=/home/sa_cluster/howhow/loki
+  cd $howhowhome
+  mkdir -p  $howhowhome/promtail/bin && mkdir -p $howhowhome/promtail/log && mkdir -p $howhowhome/loki/promtail/conf
   [[ "$?" == "0" ]] && log_info "創建 bin log conf 目錄.." || \
   log_err "創建 bin log conf 目錄失敗，請檢查權限...\n程式結束" || exit 1
-  log_info "下載promtail主程式..請稍等"
-  cd promtail/bin && wget -q  https://github.com/grafana/loki/releases/download/v2.2.1/promtail-linux-amd64.zip\
+  log_info "下載 Promtail 主程式..請稍等"
+  cd $howhowhome/promtail/bin && wget -q  https://github.com/grafana/loki/releases/download/v2.2.1/promtail-linux-amd64.zip\
    -O promtail-linux-amd64.zip && unzip "promtail-linux-amd64.zip"  > /dev/null
   chmod 755 promtail-linux-amd64
-  log_info "下載promtail組態檔及執行腳本..請稍等"
-  cd ../conf && wget -q https://jeffwen0105.github.io/howhow/Linux/Loki/conf/config-promtail.yml  -O config-promtail.yml
+  log_info "下載 Promtail 組態檔及執行腳本..請稍等"
+  cd $howhowhome/promtail/conf && wget -q https://jeffwen0105.github.io/howhow/Linux/Loki/conf/config-promtail.yml  -O config-promtail.yml
   sed -i 's+log/promtail.yml+/home/sa_cluster/howhow/loki/promtail/log/promtail.yml+g' config-promtail.yml
   sed -i 's+192.168.50.195+127.0.0.1+g' config-promtail.yml
   ls /sensorsdata/main/logs/sbp/web/*.log > /dev/null
   [[ "$?" == "0" ]] && \
    sed -i 's+/var/log/\*.log+/sensorsdata/main/logs/sbp/web/\*.log+g' config-promtail.yml || \
    sed -i 's+/var/log/\*.log+/data/sa_cluster/logs/sa/web/\*.log+g' config-promtail.yml
-  cd .. &&  wget -q https://jeffwen0105.github.io/howhow/Linux/Loki/script/start_promtail.sh -O start_promtail.sh \
+  cd $howhowhome/promtail &&  wget -q https://jeffwen0105.github.io/howhow/Linux/Loki/script/start_promtail.sh -O start_promtail.sh \
    && chmod 755 start_promtail.sh && wget -q https://jeffwen0105.github.io/howhow/Linux/Loki/script/stop_promtail.sh \
     -O stop_promtail.sh && chmod 755 stop_promtail.sh
   sed -i 's+./bin/promtail-linux-amd64+/home/sa_cluster/howhow/loki/promtail/bin/promtail-linux-amd64+g' start_promtail.sh  
   sed -i 's+=conf/config-promtail.yml+=/home/sa_cluster/howhow/loki/promtail/conf/config-promtail.yml+g' start_promtail.sh
-  sudo ln -s /home/sa_cluster/howhow/loki/promtail/start_promtail.sh /usr/bin/start_promtail.sh
-  sudo ln -s /home/sa_cluster/howhow/loki/promtail/stop_promtail.sh /usr/bin/stop_promtail.sh
+  sudo ln -s $howhowhome/promtail/start_promtail.sh /usr/bin/start_promtail.sh
+  sudo ln -s $howhowhome/promtail/stop_promtail.sh /usr/bin/stop_promtail.sh
   log_info "$(pwd)目錄狀態為："
   ls -lh
   log_info "\n程序執行完畢，請使用 sh start_promtail.sh 執行程序，會自動抓取 SBP 所有最新的 web Log"
-  log_info "請至Grafana -> http://$(hostname -i):3000  上配置Loki數據來源"
-  log_info "如何配置，請掃描QRcode參考HowHow的說明："
+  log_info "請至 Grafana -> http://$(hostname -i):3000  上配置 Loki 數據來源"
+  log_info "如何配置，請掃描 QRcode 參考HowHow網站的說明："
   qrencode -m 2 -t utf8 <<<  "qrencode -m 2 -t utf8 <<<  "https://jeffwen0105.com/grafana""
 }
 
